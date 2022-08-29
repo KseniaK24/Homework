f11b159d34d03f0b5e061434dd68772ecc7bcb6e
ksenia <knyazeva_2011@bk.ru>
2022-08-29 23:46:26 +0300

    Added Homework 2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+/*
+int SecondDigit (int num)
+{
+    int a, b, res;
+    a = num % 100;
+    b = num % 10;
+    res = (a-b)/10;
+    return res;
+}
+
+Console.WriteLine("Enter three-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SecondDigit(number);
+
+Console.WriteLine($"From number {number} the second digit {result}");
+*/
+
+// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int ThirdNumber(int num)
+{
+    while(num>999)
+    {
+    num = num / 10;
+    }
+    int res = num % 10;
+    return res;
+}
+
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>99)
+{
+int result = ThirdNumber(number);
+Console.WriteLine($"From number {number} the third digit {result}");
+}
+else
+Console.WriteLine($"{number} has no third digit");
+
+
+
