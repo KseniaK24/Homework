a75746d321b514834baa26767810abea23e863ad
ksenia <knyazeva_2011@bk.ru>
2022-09-28 01:01:29 +0300

    added homework 9

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -38,19 +38,18 @@ Console.Write(SumNaturalNumbers(n, m));
 */
 
 // Задача 68: Напишите программу, вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-/*
-int FunctionAckerman(int n, int m)
+
+int FunctionAckerman(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if ((m > 0) && (n == 0)) return FunctionAckerman(m - 1, 1);
-    else if ((m > 0) && (n > 0)) return FunctionAckerman(m - 1, FunctionAckerman(m, n - 1));
-    else return n + 1;
+    if ((m > 0) && (n == 0)) return FunctionAckerman(m - 1, 1);
+    if ((m > 0) && (n > 0)) return FunctionAckerman(m - 1, FunctionAckerman(m, n - 1));
+    return n+1;
 }
 
-Console.Write("Enter a value N: ");
-int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter a value M: ");
 int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a value N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-Console.Write(FunctionAckerman(n, m));
-*/
+Console.Write(FunctionAckerman(m, n));
